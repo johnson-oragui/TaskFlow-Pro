d9303c57d71230fdb86431500e7a37bed629b1af
johnson-oragui <johnson.oragui@gmail.com>
2025-04-18 18:59:54 +0100

    feat: added config

--- api/config/config.go
+++ api/config/config.go
@@ -1,1 +1,39 @@
 package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+type Config struct {
+	RedisUrl string
+	DBURL    string
+	Port     string
+	AppUrl   string
+}
+
+func Load() *Config {
+	HOST := os.Getenv("DB_HOST")
+	USER := os.Getenv("DB_USERNAME")
+	PASSWORD := os.Getenv("DB_PASSWORD")
+	NAME := os.Getenv("DB_NAME")
+	PORT := os.Getenv("DB_PORT")
+	SSLMODE := os.Getenv("DB_SSLMODE")
+	RedisUrl := os.Getenv("REDIS_URL")
+	APPPORT := os.Getenv("PORT")
+	APPURL := os.Getenv("APP_BASE_URL")
+
+	if APPPORT == "" || APPURL == "" || RedisUrl == "" || HOST == "" || USER == "" || PASSWORD == "" || NAME == "" || PORT == "" || SSLMODE == "" {
+		log.Fatalln("PORT REDIS_URL HOST , USER, PASSWORD, NAME, PORT, SSLMODE missing in configuration")
+	}
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", HOST, USER, PASSWORD, NAME, PORT, SSLMODE)
+
+	return &Config{
+		RedisUrl: RedisUrl,
+		DBURL:    dsn,
+		Port:     APPPORT,
+		AppUrl:   APPURL,
+	}
+}
