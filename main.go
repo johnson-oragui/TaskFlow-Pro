0c2a70ff39bcd1296ead174d226d3ae36a208a32
johnson-oragui <johnson.oragui@gmail.com>
2025-04-18 16:26:18 +0100

    feat: initialized database connection

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"log"
+
+	"github.com/johnson-oragui/TaskFlow-Pro/api/database"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Println(".env file not found, continuing...")
+
+	}
+
+}
+
 func main() {
+
+	_, err := database.ConnectDatabase()
+	if err != nil {
+		log.Println(err.Error())
+	}
 
 }
